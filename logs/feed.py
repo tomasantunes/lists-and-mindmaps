a10808ca55e33385df8bd4e1098445faf0b8a9bf
Tomás Antunes <tomasantunes@gmail.com>
2021-06-24 10:02:59 +0100

    Add files

---
+++ logs/feed.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import datetime
+import sys
+
+if (len(sys.argv) >= 2):
+	time = str(datetime.datetime.now().replace(microsecond=0))
+	text = sys.argv[1]
+	if (text == "-l"):
+		file_r = open("feed.txt", "r")
+		print(file_r.read())
+		file_r.close()
+	else:
+		file = open("feed.txt", "a")
+		file.write(time + " - " + text + "\n")
+		file.close()
+else:
+	print("Invalid arguments.")
